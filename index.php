73cb3fa5d2e32dd88e0daf3572d75bc85cff7b83
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-08 17:42:01 -0400

    added an index file and defined a route

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: homefolder
+ * Date: 2019-04-08
+ * Time: 14:40
+ */
+//Turn on error reporting
+ini_set('display_errors' ,1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class
+$f3 = Base:: instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    echo '<h1>Howdy!</h1>';
+});
+
+//run Fat-free
+$f3 -> run();
